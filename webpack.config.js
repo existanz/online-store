249ca48723a6ea2cdf17f33f1e6d296c986c1d31
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-14 15:29:47 +0300

    init: install environment 	new file:   .eslintrc.json 	new file:   .gitignore 	new file:   .prettierrc 	new file:   package.json 	new file:   src/index.ts 	new file:   tsconfig.json 	new file:   webpack.config.js 	new file:   webpack.dev.config.js 	new file:   webpack.prod.config.js

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            { 
+                test: /\.ts$/i, 
+                use: 'ts-loader' 
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' })
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
