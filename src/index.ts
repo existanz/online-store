6b61866172d510ed9604a6527c9eaea563df691d
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-12-14 20:45:57 +0700

    fix: add gh-pages, and 2 commands
    Добавил пакет gh-pages для автоматического деплоя в ветку gh-pages,
    опустил папку dist на один уровень с src, добавил 2 команды npm run
    deploy для деплоя и npm run fix для исправлений ошибок линтера

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
 console.log('Its Strart!');
-
-const func = (param: string): number => param.length;
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const baseConfig = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
