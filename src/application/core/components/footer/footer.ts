5dc1c688217baaf79a1ea8703f040502d93c2d21
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-17 16:54:44 +0300

    feat: add github logos to footer

--- src/application/core/components/footer/footer.ts
+++ src/application/core/components/footer/footer.ts
@@ -1,10 +1,15 @@
 import './footer.scss';
 import { DOMElement } from '../../../shared/components/DOMElement';
+import { GitHubLink } from './gitHubLink';
 
 export class Footer extends DOMElement {
-  gitHub: DOMElement = new DOMElement(this.node, 'a', ['footer__link']);
+  gitHub1: GitHubLink;
+  gitHub2: GitHubLink;
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'footer', ['footer'], 'this.gitHub');
+
+    this.gitHub1 = new GitHubLink(this.node);
+    this.gitHub2 = new GitHubLink(this.node);
   }
 }
