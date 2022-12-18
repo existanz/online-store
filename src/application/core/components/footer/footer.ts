08d8cee89b6abf107fc2896c12469cd19952430f
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-18 13:02:18 +0300

    feat: create footer to app

--- src/application/core/components/footer/footer.ts
+++ src/application/core/components/footer/footer.ts
@@ -1,15 +1,22 @@
 import './footer.scss';
 import { DOMElement } from '../../../shared/components/DOMElement';
-import { GitHubLink } from './gitHubLink';
+import { SVGLink } from './SVGLink';
+import { SVGs } from './SVGs';
 
 export class Footer extends DOMElement {
-  gitHub1: GitHubLink;
-  gitHub2: GitHubLink;
+  gitHub1: SVGLink;
+  gitHub2: SVGLink;
+  year: DOMElement;
+  rsschool: SVGLink;
+  ghSVG: string = SVGs.githubSVG;
+  rsschoolSVG: string = SVGs.rsschoolSVG;
 
   constructor(parentNode: HTMLElement) {
-    super(parentNode, 'footer', ['footer'], 'this.gitHub');
+    super(parentNode, 'footer', ['footer']);
 
-    this.gitHub1 = new GitHubLink(this.node);
-    this.gitHub2 = new GitHubLink(this.node);
+    this.gitHub1 = new SVGLink(this.node, 'https://github.com/chervyakov-vladislav', 'Владислав Червяков', this.ghSVG);
+    this.gitHub2 = new SVGLink(this.node, 'https://github.com/EXisTAnZ', 'Озиев Магомед', this.ghSVG);
+    this.year = new DOMElement(this.node, 'p', ['footer__year'], '2023');
+    this.rsschool = new SVGLink(this.node, 'https://rs.school/js/', '', this.rsschoolSVG);
   }
 }
