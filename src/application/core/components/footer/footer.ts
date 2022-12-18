27ace02fc095945f6663703c4dda8b1570bc93ce
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-18 14:47:03 +0300

    fix: change styles according to layout

--- src/application/core/components/footer/footer.ts
+++ src/application/core/components/footer/footer.ts
@@ -4,6 +4,8 @@ import { SVGLink } from './SVGLink';
 import { SVGs } from './SVGs';
 
 export class Footer extends DOMElement {
+  container: DOMElement;
+  footerLinx: DOMElement;
   gitHub1: SVGLink;
   gitHub2: SVGLink;
   year: DOMElement;
@@ -14,14 +16,16 @@ export class Footer extends DOMElement {
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'footer', ['footer']);
 
+    this.container = new DOMElement(this.node, 'div', ['container', 'footer__container']);
+    this.footerLinx = new DOMElement(this.container.node, 'div', ['footer-links']);
     this.gitHub1 = new SVGLink(
-      this.node,
+      this.footerLinx.node,
       'https://github.com/chervyakov-vladislav',
       'Vladislav Chervyakov',
       this.ghSVG
     );
-    this.gitHub2 = new SVGLink(this.node, 'https://github.com/EXisTAnZ', 'Magomed Oziev', this.ghSVG);
-    this.year = new DOMElement(this.node, 'p', ['footer__year'], '2023');
-    this.rsschool = new SVGLink(this.node, 'https://rs.school/js/', '', this.rsschoolSVG);
+    this.gitHub2 = new SVGLink(this.footerLinx.node, 'https://github.com/EXisTAnZ', 'Magomed Oziev', this.ghSVG);
+    this.year = new DOMElement(this.container.node, 'p', ['footer__year'], '2023');
+    this.rsschool = new SVGLink(this.container.node, 'https://rs.school/js/', '', this.rsschoolSVG);
   }
 }
