ee86c52f76db5936aad7f3684ddf99ad537bd7c5
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-12-23 21:59:16 +0700

    feat: add store page

--- src/application/main/components/store-page/items/grid-card.ts
+++ src/application/main/components/store-page/items/grid-card.ts
@@ -2,12 +2,18 @@ import './grid-card.scss';
 import { ProductsData } from '../../../../shared/models/response-data';
 import { DOMElement } from '../../../../shared/components/base-elements/dom-element';
 import { ImageElement } from '../../../../shared/components/base-elements/image-element';
+import { ButtonElement } from '../../../../shared/components/base-elements/button-element';
 
 export class GridCard {
   private card: DOMElement;
   private imageContainer: DOMElement;
   private image: ImageElement;
   private description: DOMElement;
+  private text: DOMElement;
+  private buttons: DOMElement;
+  private button: ButtonElement;
+  private title: DOMElement;
+  private price: DOMElement;
 
   constructor(parentNode: HTMLElement, product: ProductsData | null) {
     this.card = new DOMElement(parentNode, {
@@ -30,6 +36,33 @@ export class GridCard {
     this.description = new DOMElement(this.card.node, {
       tagName: 'div',
       classList: ['grid-card__description'],
+    });
+
+    this.text = new DOMElement(this.description.node, {
+      tagName: 'div',
+      classList: ['grid-card__text'],
+    });
+
+    this.buttons = new DOMElement(this.description.node, {
+      tagName: 'div',
+      classList: ['grid-card__buttons'],
+    });
+
+    this.button = new ButtonElement(this.buttons.node, {
+      tagName: 'button',
+      classList: ['grid-card__button'],
+    });
+
+    this.title = new DOMElement(this.text.node, {
+      tagName: 'h3',
+      classList: ['grid-card__title'],
+      content: product?.title,
+    });
+
+    this.price = new DOMElement(this.text.node, {
+      tagName: 'p',
+      classList: ['grid-card__price'],
+      content: `$${product?.price}`,
     });
   }
 }
