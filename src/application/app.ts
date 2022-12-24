4064d7a737f23215700fbd895cf032561e88ffb7
EXisTAnZ <ozim_ssspam@mail.ru>
2022-12-24 12:54:07 +0300

    Feat/local storage (#11)
    * feat: implement local-storage-service
    
    * feat: add getRecordObj from localstorage
    
    * feat: add types for methods
    
    * fix: remove debug scripts from app.ts
    
    * fix: remove assertion

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,11 +1,20 @@
 import { Header } from './core/components/header/header';
 import { Footer } from './core/components/footer/footer';
 import { Main } from './core/components/main-container/main-container';
+import LocalStorageSvc from './shared/services/local-storage.service';
 
 class App {
-  private header: Header = new Header(document.body);
-  private main: Main = new Main(document.body);
-  private footer: Footer = new Footer(document.body);
+  private header: Header;
+  private main: Main;
+  private footer: Footer;
+  public localStorageSvc;
+
+  constructor() {
+    this.header = new Header(document.body);
+    this.main = new Main(document.body);
+    this.footer = new Footer(document.body);
+    this.localStorageSvc = new LocalStorageSvc();
+  }
 
   private render(): void {
     // рендер остального содержимого относительно кверизапроса, стостояния localStorage и.т.д.
