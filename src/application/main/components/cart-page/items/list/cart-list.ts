36133d55007c1931ff579b9401d326a232a6482a
EXisTAnZ <ozim_ssspam@mail.ru>
2023-01-03 23:46:11 +0300

    feat: dinamic update cart render

--- src/application/main/components/cart-page/items/list/cart-list.ts
+++ src/application/main/components/cart-page/items/list/cart-list.ts
@@ -1,6 +1,7 @@
 import { DOMElement } from '../../../../../shared/components/base-elements/dom-element';
 import { ProductsData } from '../../../../../shared/models/response-data';
 import { CartItem } from './cart-item';
+import { State } from '../../../../../shared/services/state.service';
 import './cart-list.scss';
 
 export class CartList extends DOMElement {
@@ -13,9 +14,11 @@ export class CartList extends DOMElement {
     if (data) {
       this.render(data);
     }
+    this.node.addEventListener('click', () => this.render(State.cart));
   }
 
   public render(data: ProductsData[]) {
+    this.node.innerHTML = '';
     data.map((product, index) => new CartItem(this.node, product, index));
   }
 }
