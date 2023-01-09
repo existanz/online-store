ee642983d6e0c937bd2424fd448bad0398881d37
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2023-01-09 13:15:09 +0300

    feat: add message when cart is empty

--- src/application/main/components/cart-page/items/items.ts
+++ src/application/main/components/cart-page/items/items.ts
@@ -1,12 +1,14 @@
 import { DOMElement } from '../../../../shared/components/base-elements/dom-element';
 import { ProductsData } from '../../../../shared/models/response-data';
+import { State } from '../../../../shared/services/state.service';
 import './items.scss';
 import { CartList } from './list/cart-list';
 import { Pagination } from './pagination/pagination';
 
 export class CartItems extends DOMElement {
   private pagination: Pagination;
-  private list: CartList;
+  private cartList: CartList;
+  private emptyBag: DOMElement;
 
   constructor(parentNode: HTMLElement, data: ProductsData[]) {
     super(parentNode, {
@@ -15,11 +17,19 @@ export class CartItems extends DOMElement {
     });
 
     this.pagination = new Pagination(this.node, data);
-    this.list = new CartList(this.node, data);
+    this.cartList = new CartList(this.node, data);
+    this.emptyBag = new DOMElement(this.node, {
+      tagName: 'p',
+      classList: ['cart-items__empty', 'cart-items__empty--visible'],
+      content: 'There is no goods in bag. Please go to Store and buy something',
+    });
+    this.render();
   }
 
   public render() {
+    if (State.cart.length == 0) this.emptyBag.node.classList.add('cart-items__empty--visible');
+    else this.emptyBag.node.classList.remove('cart-items__empty--visible');
     this.pagination.render();
-    this.list.render();
+    this.cartList.render();
   }
 }
