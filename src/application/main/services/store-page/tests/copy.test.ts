94118daf4c4010ef561dbddbfa40597fc5a1933a
Vladislav Chervyakov <chervyakovve@mail.ru>
2023-01-10 19:58:02 +0700

    feat: tmp result

---
+++ src/application/main/services/store-page/tests/copy.test.ts
@@ -0,0 +1,13 @@
+import { CopyService } from "../filters/copy.service";
+
+
+// describe('test Copy url', () => {
+//   const button = document.createElement('button');
+//   button.innerText = 'текст'
+
+//   it('меняет текст кнопки на 2 секунды и копирует содержимое адресной строки', () => {
+//     CopyService.copy(button);
+//     expect(button.innerText).toEqual('Copied!');
+//     setTimeout(() => expect(button.innerText).toEqual('Copy'), 3000);
+//   })
+// });
