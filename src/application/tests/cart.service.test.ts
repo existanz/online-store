5b60d41d24b577df7cb0ffa5d8299a8d050f77b9
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2023-01-13 19:24:18 +0300

    feat: add tests for cartsevice isPromo isActivePromo methods

--- src/application/tests/cart.service.test.ts
+++ src/application/tests/cart.service.test.ts
@@ -10,16 +10,17 @@ const cartService = new CartService();
 describe('PromoCodes testing', () => {
   it('activate promo', () => {
     cartService.activePromo = [];
+    const len = cartService.activePromo.length;
     cartService.activatePromo('RS');
     const result: string = cartService.activePromo[0];
     const expected = 'RS';
     expect(result).toBe(expected);
     cartService.activatePromo('RS');
-    expect(cartService.activePromo.length).toBe(1);
+    expect(cartService.activePromo.length).toBe(len + 1);
     cartService.activatePromo('EPAM');
-    expect(cartService.activePromo.length).toBe(2);
+    expect(cartService.activePromo.length).toBe(len + 2);
     cartService.activatePromo('WRONG');
-    expect(cartService.activePromo.length).toBe(2);
+    expect(cartService.activePromo.length).toBe(len + 2);
   }),
     it('deactivate promo', () => {
       const len = cartService.activePromo.length;
@@ -29,5 +30,19 @@ describe('PromoCodes testing', () => {
       expect(cartService.activePromo.length).toBe(len - 1);
       cartService.deactivatePromo('EPAM');
       expect(cartService.activePromo.length).toBe(len - 2);
+    }),
+    it('is Promo?', () => {
+      expect(cartService.isPromo('WRONG')).toBe(false);
+      expect(cartService.isPromo('RS')).toBe(true);
+      expect(cartService.isPromo('EPAM')).toBe(true);
+    }),
+    it('is active Promo?', () => {
+      expect(cartService.isActivePromo('WRONG')).toBe(false);
+      expect(cartService.isActivePromo('RS')).toBe(false);
+      expect(cartService.isActivePromo('EPAM')).toBe(false);
+      cartService.activatePromo('RS');
+      cartService.activatePromo('EPAM');
+      expect(cartService.isActivePromo('RS')).toBe(true);
+      expect(cartService.isActivePromo('EPAM')).toBe(true);
     });
 });
